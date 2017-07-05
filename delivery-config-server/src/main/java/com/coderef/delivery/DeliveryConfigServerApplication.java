9f1dad0465ae4da1905e7a62cf1564dcc229ba22
João Rafael <jrafael_campossilva@hotmail.com>
2017-07-05 20:15:38 -0300

    Adicionando anotação do Spring Cloud Config

--- delivery-config-server/src/main/java/com/coderef/delivery/DeliveryConfigServerApplication.java
+++ delivery-config-server/src/main/java/com/coderef/delivery/DeliveryConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.coderef.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class DeliveryConfigServerApplication {
 
 	public static void main(String[] args) {
