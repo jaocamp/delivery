07cc2511ae685c2279363ec220b040172efa1940
João Rafael <jrafael_campossilva@hotmail.com>
2017-07-05 19:25:28 -0300

    Servidor de configuração configurado

---
+++ delivery-config-server/src/main/java/com/coderef/delivery/DeliveryConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.coderef.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryConfigServerApplication.class, args);
+	}
+}
