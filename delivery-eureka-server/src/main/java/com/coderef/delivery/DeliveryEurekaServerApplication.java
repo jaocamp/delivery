389b84a003ecd89059ff6f05163762b5162822a5
Unknown <jrafael_campossilva@hotmail.com>
2017-07-22 02:36:37 -0300

    Servidor Eureka Configurado

---
+++ delivery-eureka-server/src/main/java/com/coderef/delivery/DeliveryEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.coderef.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DeliveryEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryEurekaServerApplication.class, args);
+	}
+}
