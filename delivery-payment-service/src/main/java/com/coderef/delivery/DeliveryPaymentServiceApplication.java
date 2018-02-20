aab4cff2391dd78530f540a995c197982ad3c8e5
diegosilva13 <diego.brener@contaazul.com>
2018-02-20 00:51:30 -0300

    Payment service

---
+++ delivery-payment-service/src/main/java/com/coderef/delivery/DeliveryPaymentServiceApplication.java
@@ -0,0 +1,12 @@
+package com.coderef.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryPaymentServiceApplication.class, args);
+	}
+}
