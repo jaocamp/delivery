4ee144db5cf69e7be80dfdae2bb46362539401e2
matheusrodrigues13 <matheusrodrigues.ec@gmail.com>
2018-03-27 22:11:36 -0300

    created delivery-payments-sevice

---
+++ delivery-payments-serivce/src/main/java/com/coderef/delivery/DeliveryPaymentsSerivceApplication.java
@@ -0,0 +1,12 @@
+package com.coderef.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryPaymentsSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryPaymentsSerivceApplication.class, args);
+	}
+}
